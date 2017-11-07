f7ac3a289ab9d397dd029d11094746b2d0d54f10
andy-bst <andy@bluestacks.com>
2017-11-07 16:59:37 +0800

    增加遗传算法，非常强大

--- PCI/optimization.py
+++ PCI/optimization.py
@@ -32,6 +32,8 @@ def schedulecost(sol):
     totalprice = 0
     latestarrival = 0
     earliestdep = 24 * 60
+    if not sol:
+        print ("qq")
 
     for d in range(len(sol) / 2):
         # Get the inbound and outbound flights
@@ -187,8 +189,88 @@ def annealingoptimize(domain, costf, T=100000.0, cool=0.999, step=1):
     return vec
 
 
+# domain = [(0, 9)] * (len(people) * 2)
+# s = annealingoptimize(domain, schedulecost)
+# print(s)
+# print(schedulecost(s))
+# printschedule(s)
+
+
+def geneticoptimize(domain, costf, popsize=50, step=1,
+                    mutprob=0.2, elite=0.2, maxiter=100):
+    # Mutation Operation
+    def mutate(vec):
+        i = random.randint(0, len(domain) - 1)
+        if random.random() < 0.5 and vec[i] > domain[i][0]:
+            result = vec[0:i] + [vec[i] - step] + vec[i + 1:]
+        elif vec[i] < domain[i][1]:
+            result = vec[0:i] + [vec[i] + step] + vec[i + 1:]
+        else:
+            result = None
+        return result
+
+    def mutate(vec):
+        i = random.randint(0, len(domain) - 1)
+        # choice = random.choice([-1,1])
+        choices = []
+        if vec[i] > domain[i][0]:
+            #"可以变小"
+            choices.append(-1)
+        if vec[i] < domain[i][1]:
+            #"可以变大"
+            choices.append(1)
+        choice = random.choice(choices)
+        result = vec[0:i] + [vec[i] + (choice*step)] + vec[i + 1:]
+        if result is None:
+            print( "error")
+        return result
+    # Crossover Operation
+    def crossover(r1, r2):
+        i = random.randint(1, len(domain) - 2)
+        return r1[0:i] + r2[i:]
+
+    # Build the initial population
+    pop = []
+    for i in range(popsize):
+        vec = [random.randint(domain[i][0], domain[i][1])
+               for i in range(len(domain))]
+        pop.append(vec)
+
+    # How many winners from each generation?
+    topelite = int(elite * popsize)
+
+    # Main loop
+    for i in range(maxiter):
+        scores = [(costf(v), v) for v in pop]
+        scores.sort()
+        ranked = [v for (s, v) in scores]
+
+        # Start with the pure winners
+        pop = ranked[0:topelite]
+
+        # Add mutated and bred forms of the winners
+        while len(pop) < popsize:
+            if random.random() < mutprob:
+
+                # Mutation
+                c = random.randint(0, topelite)
+                tmp = mutate(ranked[c])
+                pop.append(tmp)
+            else:
+
+                # Crossover
+                c1 = random.randint(0, topelite)
+                c2 = random.randint(0, topelite)
+                tmp = crossover(ranked[c1], ranked[c2])
+                pop.append(tmp)
+
+        # Print current best score
+        print("gen number:",i, scores[0][0])
+
+    return scores[0][1]
+
+
 domain = [(0, 9)] * (len(people) * 2)
-s = annealingoptimize(domain, schedulecost)
-print(s)
-print(schedulecost(s))
-printschedule(s)
+print (geneticoptimize(domain, schedulecost, popsize=1000, mutprob=0.2, elite=0.2, maxiter=50))
+
+
