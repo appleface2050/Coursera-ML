8fffaa41618b07212d2bb48fa8f9538b579bbf60
andy-bst <andy@bluestacks.com>
2018-01-11 17:10:13 +0800

    1

--- keras_test/mnist_mlp_pca.py
+++ keras_test/mnist_mlp_pca.py
@@ -20,6 +20,7 @@ import datetime
 
 batch_size = 128
 num_classes = 10
+epochs = 30
 
 # the data, shuffled and split between train and test sets
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
